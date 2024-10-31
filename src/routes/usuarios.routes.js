954725bbf3e8bc805eb11b985105371931319089
Rayssa <158166558+raygoncalvesss@users.noreply.github.com>
2024-10-31 09:28:33 -0300

    feat: create users repository

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
 
-let usuarios = []
+const usersRepository = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
+    const usuarios = usersRepository.getAllUsers();
     return res.status(200).json({
         message: 
         usuarios.length == 0
-        ? "Não há usuarios cadastrados" : `Total de usuarios: ${usuarios.length}`,
+        ? "Não há usuarios cadastrados"
+        : `Total de usuarios: ${usuarios.length}`,
         usuarios,
     });
 });
